9731a0278352e85282dd067698e691025656e74f
Ashutosh Gupta <ashutoshgupta8199@gmail.com>
2026-09-25 20:06:12 +0530

    Time: 2 ms (77.27%), Space: 12.4 MB (21.21%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -4,13 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ln=len(nums)
-        for i in range(ln):
-            n,s= nums[i],0        
-            for j in str(n):
-                s += int(j)
-            if s == i:
-                return i            
+        if nums[0]==0:
+            return 0
+        for i in range(1, len(nums)):
+            if nums[i]>=10:
+                n = nums[i]
+                sum_d = 0
+                while n>0:
+                    sum_d+=n%10
+                    n//=10
+                if sum_d == i:
+                    return i
+                else:
+                    continue
+            if nums[i]==i:
+                return i
         return -1
-        __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
+
+
+        
         
